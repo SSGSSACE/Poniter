a3d154c95f1704e3dbb5e1138859e712fe4d1415
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 18:51:53 +0700

    June 20, 2021 6:51 PM

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
     std::cout << "Gia tri cua p la: " << p << std::endl;
     std::cout << "Dia chi cua bien p la: " << &p << std::endl;
     std::cout << "Gia tri cua bien con tro dang tro toi la: " << *p << std::endl;
+    std::cout << "/* message */" << std::endl;
     *p = 20;
     std::cout << "-------------SAU DO--------------" << std::endl;
     std::cout << "Gia tri cua x la: " << x << std::endl;
