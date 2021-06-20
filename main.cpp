a66bc810ac1cff6ad56b4031f9eb1ac896000788
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 17:13:29 +0700

    June 20, 2021 5:13 PM

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
-    int x=10;
-    std::cout << "Gia tri cua x la: " <<x<< std::endl;
-    std::cout << "/* message */" << std::endl;
+    int x = 10;
+    std::cout << "Gia tri cua x la: " << x << std::endl;
+    std::cout << "Dia chi cua bien x la: " << &x << std::endl;
     return 0;
 }
