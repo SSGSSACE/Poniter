ceba25c0d0e8b92dd7ec0e13c4da295fa899aab1
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 17:39:53 +0700

    June 20, 2021 5:39 PM

--- main.cpp
+++ main.cpp
@@ -14,6 +14,6 @@ int main()
     std::cout << "Gia tri cua x la: " << x << std::endl;
     std::cout << "Dia chi cua bien x la: " << &x << std::endl;
     std::cout << "Gia tri cua p la: " << p << std::endl;
-    std::cout << "Dia chi cua bien p la: " << *p << std::endl;
+    std::cout << "Dia chi cua bien p la: " << &p << std::endl;
     return 0;
 }
