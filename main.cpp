b3996bf3363ee3d7f05783a630b9ebbbb2dc634b
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 17:20:14 +0700

    June 20, 2021 5:20 PM

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     int x = 10;
+    int *p=&x;
     std::cout << "Gia tri cua x la: " << x << std::endl;
     std::cout << "Dia chi cua bien x la: " << &x << std::endl;
     return 0;
