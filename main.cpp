151c255d41af098213b20f55c9199785fe0c2016
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 17:44:27 +0700

    June 20, 2021 5:44 PM

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     int x = 10;
-    int *p=&x;
+    int *p = &x;
     std::cout << "Gia tri cua x la: " << x << std::endl;
     std::cout << "Dia chi cua bien x la: " << &x << std::endl;
     std::cout << "Gia tri cua p la: " << p << std::endl;
     std::cout << "Dia chi cua bien p la: " << &p << std::endl;
     std::cout << "Gia tri cua bien con tro dang tro toi la: " << *p << std::endl;
-    *p=20;
+    *p = 20;
     std::cout << "-------------SAU DO--------------" << std::endl;
     std::cout << "Gia tri cua x la: " << x << std::endl;
     std::cout << "Dia chi cua bien x la: " << &x << std::endl;
