f38e5ae8c9fcdd7665b17016933aa08bc74828e8
SSGSSACE <quangbayhai@gmail.com>
2021-06-20 17:43:07 +0700

    June 20, 2021 5:43 PM

--- main.cpp
+++ main.cpp
@@ -9,12 +9,13 @@ int main()
     std::cout << "Dia chi cua bien x la: " << &x << std::endl;
     std::cout << "Gia tri cua p la: " << p << std::endl;
     std::cout << "Dia chi cua bien p la: " << &p << std::endl;
-    std::cout << "Gia tri cua bienDia chi cua bien p la: " << &p << std::endl;
+    std::cout << "Gia tri cua bien con tro dang tro toi la: " << *p << std::endl;
     *p=20;
     std::cout << "-------------SAU DO--------------" << std::endl;
     std::cout << "Gia tri cua x la: " << x << std::endl;
     std::cout << "Dia chi cua bien x la: " << &x << std::endl;
     std::cout << "Gia tri cua p la: " << p << std::endl;
     std::cout << "Dia chi cua bien p la: " << &p << std::endl;
+        std::cout << "Gia tri cua bien con tro dang tro toi la: " << *p << std::endl;
     return 0;
 }
